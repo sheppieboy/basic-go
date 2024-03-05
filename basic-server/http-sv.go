82a669c4ec44c182b04a9a52138c9c4ed4dd3ece
Luke Sheppard <sheppiegit@gmail.com>
2024-03-05 17:39:25 -0500

    refactored basic server to get from josntypicode and parse it as a html using template and serve it on localhost.

--- basic-server/http-sv.go
+++ basic-server/http-sv.go
@@ -1,13 +1,60 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"html/template"
+	"io"
 	"log"
 	"net/http"
 )
 
+type todo struct {
+	UserID    int    `json:"userID"`
+	ID        int    `json:"id"`
+	Title     string `json:"title"`
+	Completed bool   `json:"completed"`
+}
+
+
+var form = `
+<h1>Todo #{{.ID}}<h1>
+<div>{{printf "User %d" .UserID}}<div/>
+<div>{{printf "%s (completed: %t)" .Title .Completed}}<div/>
+`
+
+
 func handler(w http.ResponseWriter, r *http.Request){
-	fmt.Fprintf(w, "hello, world! from %s\n", r.URL.Path[1:])
+	const base = "https://jsonplaceholder.typicode.com/" //return to us some json
+
+	resp, err := http.Get(base + r.URL.Path[1:])
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusServiceUnavailable)
+		return
+	}
+
+	//defer will wait for response till after
+	defer resp.Body.Close() //response could have body, we need to close it as socket wont close
+
+	body, err := io.ReadAll(resp.Body)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	var item todo
+
+	err = json.Unmarshal(body, &item)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	tmpl := template.New("mine")
+	tmpl.Parse(form)
+	tmpl.Execute(w, item)
+
 }
 
 func main(){
