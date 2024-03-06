c95006dbe70cacc24aa87759d5e59782ddc9eb57
Luke Sheppard <sheppiegit@gmail.com>
2024-03-06 11:09:35 -0500

    added in ScalebyX

--- methods-interfaces.go
+++ methods-interfaces.go
@@ -20,23 +20,15 @@ func (l Line) Distance() float64{
 	return math.Hypot(l.End.X - l.Begin.X, l.End.Y - l.Begin.Y)
 }
 
-func (p Path) Distance() (sum float64){
-	for i := 1; i<len(p); i++{
-		sum += Line{p[i-1], p[i]}.Distance()
-	}
-	return sum
-}
-type Distancer interface {
-	Distance() float64
-}
-func PrintDistance(d Distancer){
-	fmt.Println((d.Distance()))
+func (l *Line) ScaleByX(f float64){
+	l.End.X += (f-1)*(l.End.X - l.Begin.X)
+	l.End.Y += (f-1)*(l.End.Y - l.Begin.Y)
 }
 
 func main(){
 	side := Line{Point{1,2}, Point{4,6}}
-	perimeter := Path{{1,1}, {5,1}, {5,4}, {1,1}}
 	fmt.Println(side.Distance())
-	fmt.Println(perimeter.Distance())
-	PrintDistance(side)
+	side.ScaleByX(2)
+	fmt.Println(side.Distance())
+
 }
