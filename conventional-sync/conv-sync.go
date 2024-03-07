f0e5f4882ea6914379da3b4115af28dc2d4f4950
Luke Sheppard <sheppiegit@gmail.com>
2024-03-07 12:34:04 -0500

    channel acting as mutex.

--- conventional-sync/conv-sync.go
+++ conventional-sync/conv-sync.go
@@ -6,13 +6,17 @@ import (
 )
 
 func do() int{
+	m := make(chan bool, 1)
+
 	var n int64
 	var w sync.WaitGroup
 
 	for i := 0; i < 1000; i++{
 		w.Add(1)
 		go func(){
+			m <- true
 			n++
+			<-m
 			w.Done()
 		}()
 	}
